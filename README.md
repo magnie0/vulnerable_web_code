04a19ca76f0a7ea11f5a91fabbc5c764abb50f0e
magnie0 <magdalenanieszporska@gmail.com>
2024-06-05 22:05:06 +0200

    Added second challenge

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # vulnerable_web_code
+
+To run app create virtual environment
+python3 -m venv .venv
+
+Activate virtual environment
+source .venv/bin/activate
+
+Install pip packages
+pip install mysqlclient
+pip install mysql-connector-python
+pip install flask
+
+
+Create database in mysql:
+CREATE USER 'testuser'@'localhost' IDENTIFIED WITH authentication_plugin BY 'testuser';
+CREATE DATABASE webServer;
+USE webServer;
+CREATE TABLE users (username VARCHAR(30) NOT NULL, password VARCHAR(30) NOT NULL);
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, send_from_directory, render_template, abort
+from flask import Flask, request, send_from_directory, render_template, abort, flash
 import os
-
+import mysql.connector
 app = Flask(__name__, static_url_path = "/images", static_folder = "images")
 
 # Directory where files are stored for downloading
@@ -35,7 +35,37 @@ def download_file(filename):
     except Exception as e:
         abort(500, str(e))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        #Connection as in https://knowledge-base.secureflag.com/vulnerabilities/sql_injection/sql_injection_python.html
+        # Prepare database connection
+        #db = pymysql.connect( user='root', host = 'localhost', passwd='')
+
+        
+        connection = mysql.connector.connect(database='webServer', host = 'localhost',
+                                            user='testuser',password='testuser')
+        cursor = connection.cursor()
+
+        # Execute the vulnerable SQL query concatenating user-provided input.
+        sqlRequest =  "select * from users where username ='"+username+"'and password ='"+password+"'"
+
+        cursor.execute(sqlRequest)
+        # If the query returns any matching record, consider the current user logged in.
+        record = cursor.fetchone()
+        #record = True
+        if record:
+            flash('Login successful! FLAG#wefrwaoipejprow3opo#', 'success')
+            #return redirect(url_for('index'))
+        else:
+            flash('Login failed. Check your username and/or password.', 'danger')
+    return render_template('challenge2.html')
+
 if __name__ == '__main__':
+    app.secret_key = 'secretkey' #shouldnt be here in production
     app.run(debug=True)
 
 #path traversal: curl --path-as-is http://127.0.0.1:5000/files/../../../../../../../etc/passwd
